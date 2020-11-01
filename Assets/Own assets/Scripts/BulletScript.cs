cc01d8e7e5e4e3d103494e69b0ab7ca26c0c4a48
tinahauq <40709994+tinahauq@users.noreply.github.com>
2020-11-01 16:30:27 +0100

    Test
    First commit test

---
+++ Assets/Own assets/Scripts/BulletScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour
+{
+    //Declaratie en initialisatie van de public variables.
+
+    public float velX; // De horizontale snelheid krijgt een waarde via de gebruikte prefab. Voor een BulletToLeft wordt dit -5 zodat 
+                       // de Bullet naar links zal bewegen langs de X-as. Voor een BulletToRight staat de waarde ingesteld op 5 zodat
+                       // de bullet naar rechts beweegt langs de X-as. (Je stelde deze waarden reeds in toen je de prefabs maakte).
+
+    public float velY = 0f; // De verticale snelheid is 0 zodat de bullet niet naar beneden valt.
+
+    //Declaratie variabele van het type Rigidbody om de RigidBody component van een Bullet kunnen aan te spreken in wat volgt.
+
+    Rigidbody2D rb; 
+
+    void Awake() //De functie Awake wordt uitgevoerd als het object gemaakt wordt.
+    {
+        rb = GetComponent<Rigidbody2D>(); //De variabele rb wordt gekoppeld aan de Rigidbody2D component
+                                          //waaraan dit script gekoppeld is, dus aan de Rigidbody2D van een Bullet.
+    }
+
+   
+    void Update()
+    {
+        rb.velocity = new Vector2(velX, velY); //De property velocity geeft de snelheid aan waarmee de kogel binnen elk frame bewogen wordt. 
+                                               //Deze instructie zorgt er dus voor dat de kogel blijft voortbewegen tegen de ingestelde snelheid.
+    }
+}
---
+++ Assets/Own assets/Scripts/PlayerControl.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public float speed = 1.5f;
+
+    //Variabele die zal geïnitialiseerd worden op de SpriteRenderer component van de Player.
+    //Via deze variabele kunnen we de property flipX aanspreken die we nodig hebben om de sprite
+    //te flippen op de X-as.
+    private SpriteRenderer mySpriteRenderer;
+
+    //Public variables die we via het Inspector venster instellen op de prefabs (selecteer het Player object
+    //om deze te kunnen instellen bij de component Player Control (Script)).
+    //In de Fire() functie gebruiken we deze variabelen om van de ingestelde prefab een kogel te maken.
+    public GameObject bulletToRight, bulletToLeft;
+
+    //Variabele die in de functie Fire() gebruikt wordt om de startpositie van een Bullet te bepalen.
+    Vector2 bulletPos;
+
+    //Awake() wordt uitgevoerd als het GameObject gemaakt wordt.
+    private void Awake()
+    {
+        //De variabele mySpriteRenderer wordt gekoppeld aan de Sprite Renderer component van
+        //het Player object. Via deze variabele kunnen we in wat volgt de flipX property aanspreken.
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //Toegevoegd om de sprite te flippen op de X-as als er op de linkse pijltoets gedrukt wordt.
+            mySpriteRenderer.flipX = true;
+            //Einde toevoeging
+
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //Toegevoegd om de sprite origineel te tonen als er op de rechtse pijltoets gedrukt wordt.
+            //(origineel = kijkend naar rechts)
+            mySpriteRenderer.flipX = false;
+            //Einde toevoeging
+
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * speed * 2 * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
+        if (Input.GetButtonDown("Fire1")) //Wordt de Fire1-toets ingedrukt (= linkse CTRL toets)
+        {
+            Fire(); //De functie Fire wordt uitgevoerd als de Fire1 button ingedrukt wordt (dit is ingesteld op de linkse CTRL toets).
+        }
+    }
+    void Fire()
+    {
+
+        //De positie van de bullet wordt geïnitialiseerd op de positie van de Player. Transform.position geeft immers
+        //de positie van de Player aan aangezien dit script aan het Player object gekoppeld is.
+        bulletPos = transform.position;
+
+        if (mySpriteRenderer.flipX) //Indien flipX true is, kijkt de Player naar links en moet er een Bullet geïnstantieerd worden
+        {                           //die naar links gaat.
+            bulletPos += new Vector2(-0.4f, +0.05f); //De positie van de Bullet wordt iets naar links en iets naar boven verschoven t.o.v. de Player.
+                                                     //Dit geeft het effect dat de Bullet aan de linkerkant van de Player vertrekt ter hoogte van zijn hand.
+
+            Instantiate(bulletToLeft, bulletPos, Quaternion.identity); //Van de prefab bulletToLeft wordt een nieuwe instantie gemaakt
+        }                                                              //die getoond wordt op de positie bulletPos.
+        else
+        {
+            bulletPos += new Vector2(+0.4f, +0.05f); //De positie van de Bullet wordt iets naar rechts en iets naar boven verschoven t.o.v. de Player.
+                                                     //Dit geeft het effect dat de Bullet aan de rechterkant van de Player vertrekt ter hoogte van zijn hand.
+
+            Instantiate(bulletToRight, bulletPos, Quaternion.identity); //Van de prefab bulletToLeft (argument 1) wordt een nieuwe instantie gemaakt
+        }                                                               //die getoond wordt op de positie bulletPos (argument 2).
+                                                                        //De functie Instantiate heeft meerdere overloads. Hier gebruiken we de versie met
+                                                                        //3 argumenten. Het derde argument geeft de rotatie aan. We stellen dit hier in op
+                                                                        //Quaternion.identity, wat betekent: geen rotatie t.o.v. de wereld waarin het object
+                                                                        //zich bevindt.
+    }
+}
+
